3a62dcda5b1995fbee8040a332dec5caa8b0e75f
Bas <158242081+7ois@users.noreply.github.com>
2024-11-04 02:36:54 +0700

    Add MainLayout & MainHeader

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import MainLayout from "@/components/MainLayout";
 import "./globals.css";
 
 export default function RootLayout({
@@ -7,7 +8,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <MainLayout>{children}</MainLayout>
+      </body>
     </html>
   );
 }
---
+++ components/MainHeader.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MainHeader = () => {
+    return (
+        <div className='bg-black'>
+            MainHeader
+        </div>
+    )
+}
+
+export default MainHeader
